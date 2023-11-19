041120116fc69e345be3a838600db4059f69bc7b
matiasdev30 <matiasdev30@gmail.com>
2023-11-19 03:12:56 +0100

    feat: add test france

--- main.go
+++ main.go
@@ -1,6 +1,25 @@
 package main
 
-func Ola(name string) string {
-	return "Olá, " + name
+
+const espanhol = "espanhol"
+const frances = "françes"
+const prefixoFrances = "Bonjour"
+const prefixoOlaEmPt = "Olá"
+const prefixoOlaEspanhol = "Holla"
+
+func Ola(name string, idioma string) string {
+	if name == "" {
+		name = "Mundo"
+	}
+
+	if idioma == espanhol {
+		return prefixoOlaEspanhol + ", " + name 
+	}
+
+	if(idioma == frances){
+		return prefixoFrances + ", " + name
+	}
+
+	return prefixoOlaEmPt + ", " + name
 }
 
--- main_test.go
+++ main_test.go
@@ -3,11 +3,39 @@ package main
 import "testing"
 
 func TestOla(t *testing.T)  {
-	resultado := Ola("Mani")
-	esperado := "Olá, Mani"
 
-	if resultado != esperado {
-		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+	verificarTest := func(resultado string, esperado string){
+		if resultado != esperado {
+			t.Errorf("resultado '%s' esperado '%s'", resultado, esperado)
+		} 
 	}
+
+	t.Run("Diz olá quando uma string for passada", func(t *testing.T){
+		resultado := Ola("Mani", "")
+		esperado := "Olá, Mani"
+
+		verificarTest(resultado, esperado)
+	})
+
+	t.Run("Diz olá sem passar nenhum parametro", func(t *testing.T){
+		resultado := Ola("", "")
+		esperado := "Olá, Mundo"
+
+		verificarTest(resultado, esperado)
+	})
+
+	t.Run("em espanhol", func(t * testing.T){
+		resultado := Ola("Eloide", "espanhol")
+		esperado := "Holla, Eloide"
+
+		verificarTest(resultado, esperado)
+	})
+
+	t.Run("em frances", func(t * testing.T){
+		resultado := Ola("Lima", "françes")
+		esperado := "Bonjour, Lima"
+
+		verificarTest(resultado, esperado)
+	})
 }
 
