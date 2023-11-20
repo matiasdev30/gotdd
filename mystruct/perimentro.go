d66e6610a49ff0c3ecc08b934d5b1ad27b686ee2
matiasdev30 <matiasdev30@gmail.com>
2023-11-20 06:33:15 +0100

    feat: refactoring, mirror changes

--- mystruct/perimentro.go
+++ mystruct/perimentro.go
@@ -7,12 +7,21 @@ type Retangulo struct {
 	altura  float64
 }
 
+type Triangulo struct {
+    base float64
+    altura float64
+}
+
 type Circulo struct {
 	raio float64
 }
 
 type Forma interface {
     Area() float64
+}
+
+func (t Triangulo) Area() float64 {
+    return (t.base * t.altura) * 0.5
 }
 
 func (r Retangulo) Area() float64 {
--- mystruct/perimentro_test.go
+++ mystruct/perimentro_test.go
@@ -2,13 +2,16 @@ package mystruct
 
 import "testing"
 
+
+//Table drive test
 func TestPerimentro(t *testing.T) {
 	tetAreas := []struct{
 		forma Forma
 		esperado float64
 	}{
-		{Retangulo{largura: 12, altura: 6}, 72.0},
-		{Circulo{raio: 10}, 314.1592653589793},
+		{forma: Retangulo{largura: 12, altura: 6}, esperado: 72.0},
+		{forma: Circulo{raio: 10}, esperado: 314.1592653589793},
+		{forma: Triangulo{base: 12, altura: 6}, esperado: 36.0},
 	}
 
 	for _, tt := range tetAreas{
